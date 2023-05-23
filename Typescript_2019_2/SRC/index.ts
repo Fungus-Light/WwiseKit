44596fa89b2d8ada69210e462981ac8a4441acd8
Fungus Light <542461670@qq.com>
2023-05-23 17:14:39 +0800

    Update index.ts

--- Typescript_2019_2/SRC/index.ts
+++ Typescript_2019_2/SRC/index.ts
@@ -1,8 +1,6 @@
 import { DEFAULT_URL, Log, SimpleConnect, } from "Wwise/Utils";
-import { WwiseTypes } from "Wwise/WwiseObjectsReference";
 import { Connection, Session } from "autobahn";
 import { APIs_Async } from "./Wwise/waapi_apis_promise"
-import { ak_wwise_core_object_create_Args, ak_wwise_core_soundbank_setInclusions_Args } from "Wwise/waapi_apis";
 
 SimpleConnect(DEFAULT_URL, HelloWwise)
 
